bc634abfbda0fc33b6b4399d095f7b3c5c1e2ff9
John Humbles <jhumbles7029@sohamcollege.org.uk>
2014-05-15 09:42:39 +0100

    Rewrote the majority of the code.
    All changes are defined in there, so you can see the fixes I made.

--- diceRoller.py
+++ diceRoller.py
@@ -1,8 +1,8 @@
-#Coin flip program
-#Describe the purpose of this program here.
-
-inport random;time
-
+#Dice Roll program
+#This simulates the rolling of a die.
+roll = None
+import random,time
+#The above should be separated by a comma, no a semi-colon.
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
 s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
@@ -11,25 +11,37 @@ s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+    print("rolling.....")
+    roll = random.randint(0,7)
+    global roll
+#roll = rand.randing(7) should be as above or the parameters are wrong and the random statement is spelled incorrectly, as is the int suffix.
 
-
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
+def show_dice(roll):
+    if roll == 1:
+        print(s1)
+    elif roll == 2:
         print(s2)
-    else roll = 3
+    elif roll == 3:
         print(s3)
-    else roll = 4
+    elif roll == 4:
         print(s4)
-    else roll = 5
+    elif roll == 5:
         print(s5)
-    else roll = 6
+    elif roll == 6:
         print(s6)
 
-roll
+def rollDice():
+    print("rolling.....")
+    roll = random.randint(0,7)
+    time.sleep(0.5)
+    show_dice(roll)
+#All of the above needed to be elif statements, have TWO '+' signs, and a colon on the end, except the obvious differences in the last one.
+
+roll()
 time.sleep(1)
 show_dice(roll)
+#These do not need indentations.
 
+
+#There was a logic scope error in that roll() kept the value for roll, so when show_dice() was called it did not have the variable so assigned 'None'.
+#This was solved by making roll global.
